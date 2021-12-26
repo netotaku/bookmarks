71e29b69a4265ee41446a1f075006f7be64f9396
Nicholas Clarkson <nicholasclarkson@Nicholass-MacBook-Pro.local>
2021-12-26 17:04:33 +0000

    contentful lib

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <div id="nav">
+  <header>
+    <h1>H!</h1>
+  </header>
+  <menu id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
+  </menu>
   <router-view/>
 </template>
 
@@ -11,5 +14,9 @@
   margin: 0px;
   padding: 0px;
   box-sizing: border-box;
+}
+html{
+  font-family: monospace;
+  font-size: 1em;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as Contentful from 'contentful'
+import VueContentfulApi from 'vue-contentful-api'
+
+let contentful = Contentful.createClient({
+    space: 'SPACE-ID',
+    accessToken: 'ACCESS-TOKEN',
+})
 
 createApp(App).use(router).mount('#app')
+Vue.use(VueContentfulApi, contentful)
