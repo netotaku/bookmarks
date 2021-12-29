5b6fb4ba8205b39009853bc40d89a049ddf4f56e
Nicholas Clarkson <nicholasclarkson@Nicholass-MacBook-Pro.local>
2021-12-29 15:13:11 +0000

    pre refactor

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,11 +1,19 @@
 <template>
     <h2>Category Content</h2>
+    <div v-if="items.length > 0">
+      items arrived there are {{ items.length }} of them {{ items[0].fields.label }}
+    </div>
 </template>
 
 <script>
 
 export default {
-  name: 'Category',  
+  name: 'Category', 
+  // data: function(){
+  //   return {
+  //     items
+  //   }
+  // }, 
   props: [
     'items'
   ],  
@@ -16,8 +24,14 @@ export default {
     update: function(){
       this.parse(this.items[0]);
     },
-    parse: function(){
-
+    parse: function (node) {   
+      console.log('NODE: ', node);
+      //console.log(node.fields.path)   
+      // if (node.fields.categories) {
+      //   node.fields.categories.forEach((item) => {
+      //     this.parse(item);
+      //   });
+      // }
     }
   },
   watch: {
